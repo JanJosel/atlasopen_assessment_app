3c4fb871377c2a1681f7f2fca70581c23c36ba27
JanJosel <jjmcv.23@gmail.com>
2020-09-20 23:25:31 +0800

    Fixed login and chat pages

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 //HINT https://github.com/FirebaseExtended/reactfire
 import { useUser, useFirestoreCollectionData, useFirestore } from 'reactfire';
@@ -7,23 +7,130 @@ import 'firebase/auth'
 
 //HINT
 import { BrowserRouter as Router, Switch, Route, Link, useParams, useHistory } from 'react-router-dom';
-import {TextField, Button, Grid, Typography} from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import SendIcon from '@material-ui/icons/Send';
+import {TextField, Button, Grid, Typography, Paper, Box, Divider, List, ListItem, ListItemIcon, ListItemText, Avatar, Fab} from "@material-ui/core";
 
-export default function Chat() {
+
+const Chat = () => {
 
     // TODO: Display messages from chat and submit messages
+
+    // message state
+    const [message, setMessage] = useState({
+        text: '',
+        error: '',
+    });
+
+    // onChange function
+    const handleChange = e => {
+        setMessage({
+            ...message,
+            [e.target.name]: e.target.value,
+            error: '',
+        })
+    };
+
+
+    // get user
+    const user = useUser();
+    // get message collection
+    const messageCollection = useFirestore().collection('messages');
+    // get message list from firestore
+    const messages = useFirestoreCollectionData(messageCollection.orderBy("timestamp"));
+
+    const StoreMessage = async(e) => {
+        var dateNow = Date(Date.now()); 
+        var date = dateNow.toString()
+
+        //console.log(message)
+        
+        // construct message
+        const sendMessage = { uid: user.uid, name: user.displayName, message: message.text, timestamp: date }
+        // add message to collection
+        messageCollection.add(sendMessage)
+        // clear text field
+        document.getElementById('messageField').value = ''
+        
+        // add latest message to message list
+        messages.push(sendMessage)
+
+        // print messages
+        //console.log(messages)
+
+    }
+
+    // 
+    const listItems = messages.map((msg, index) => 
+        <ListItem key={index}>
+            <Grid container>
+                <ListItemText align={user.uid==msg.uid ? "right" : "left"}>
+                    <ListItemIcon>
+                        <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                        <ListItemText primary={msg.name}></ListItemText>
+                    </ListItemIcon>
+                    
+                </ListItemText>
+                
+                <Grid item xs={12}>
+                    <ListItemText align={user.uid==msg.uid ? "right" : "left"} primary={msg.message}></ListItemText>
+                </Grid>
+                <Grid item xs={12}>
+                    <ListItemText align={user.uid==msg.uid ? "right" : "left"} secondary={msg.timestamp}></ListItemText>
+                </Grid>
+            </Grid>
+        </ListItem>
+    );
+
+    const useStyles = makeStyles({
+        chatSection: {
+          width: '100%',
+          height: '80vh',
+        },
+        messageArea: {
+          height: '70vh',
+          overflowY: 'auto'
+        }
+    });
+
+    const classes = useStyles();
+
+    const divRef = useRef(null);
+
+    const scroll = useEffect(() => {
+        divRef.current.scrollIntoView({ behavior: 'smooth' });
+    });
 
     return (
         <div>
             <h1>Chat Page</h1>
 
-            <Grid container>
-            <Grid md={12}>
-            <TextField onChange={(e) => console.log(e.target.value)}/>
-            </Grid>
-            <Grid md={12}>
-            <Button variant="contained">Submit</Button>
-            </Grid>
+            <Grid container component={Paper} className={classes.chatSection}>
+                
+                <Grid item xs={12}>
+
+                    <List className={classes.messageArea}>
+
+                        <Divider />
+
+                        {listItems }
+                        <ListItemText ref={divRef}></ListItemText>
+
+                    </List>
+
+                    <Divider />
+
+                    <Grid container style={{padding: '20px'}}>
+                        <Grid item xs={11}>
+                        <TextField id='messageField' name="text" label="Type Something" onChange={handleChange} fullWidth/>
+                        </Grid>
+                        <Grid item xs={1} align="right">
+                            <Fab color="primary" aria-label="add" onClick={StoreMessage}><SendIcon /></Fab>
+                        </Grid>
+                    </Grid>
+
+                </Grid>
+                
             </Grid>
 
             <Logout />
@@ -31,3 +138,5 @@ export default function Chat() {
     )
 
 }
+
+export default Chat;
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,6 +7,15 @@ var firebaseConfig = {
     // storageBucket: "assessment-app-41d0b.appspot.com",
     // messagingSenderId: "418813227371",
     // appId: "1:418813227371:web:22828bd50e32150598dc2e"
+    
+    apiKey: "AIzaSyAAExAzl2mUxrkmCj6hsLoK2pLDDuR0aWo",
+    authDomain: "assessment-app-2.firebaseapp.com",
+    databaseURL: "https://assessment-app-2.firebaseio.com",
+    projectId: "assessment-app-2",
+    storageBucket: "assessment-app-2.appspot.com",
+    messagingSenderId: "179239164713",
+    appId: "1:179239164713:web:a2361d747deed9a6bb7538"
+
 };
 
 export default firebaseConfig;
